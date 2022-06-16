baae7c60889481fdfa5df4aaaec22a44510d691e
Mario N <m.now@protonmail.com>
2022-06-16 12:14:33 +0200

    character jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public float movementSpeed = 5;
+    public float jumpSpeed = 35f;
+
     private Rigidbody2D _rigidbody;
-    public float MovementSpeed = 5;
+    private float _jumpForce;
 
     void Start()
     {
         this._rigidbody = GetComponent<Rigidbody2D>();
+        this._jumpForce = Mathf.Sqrt(jumpSpeed * -2 * (Physics2D.gravity.y * _rigidbody.gravityScale)); 
     }
 
-    void Update()
-    {
-        this._rigidbody.velocity = new Vector2(MovementSpeed, 0);
+    void FixedUpdate() {
+        transform.Translate(new Vector3(movementSpeed * Time.deltaTime, 0, 0));
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            this._rigidbody.AddForce(new Vector2(0, this._jumpForce), ForceMode2D.Impulse);
+        } 
     }
 }
