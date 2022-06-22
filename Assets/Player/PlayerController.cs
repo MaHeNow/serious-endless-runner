1d5cb2fac15b4127a6eee8987e3b9cef4103a37b
Wassim ben salem <bswassim@gmail.com>
2022-06-22 16:31:45 +0200

    double jump
    2

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -8,33 +8,62 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 5;
     public float jumpSpeed = 35f;
 
+    public delegate void HitObstacle();
+    public static event HitObstacle OnHitObstacle;
 
     private Rigidbody2D _rigidbody;
     private float _jumpForce;
     private bool _onGround;
 
+    private bool doubleJump;
+
+
     void Start()
     {
         this._rigidbody = GetComponent<Rigidbody2D>();
-        this._jumpForce = Mathf.Sqrt(jumpSpeed * -3 * (Physics2D.gravity.y * _rigidbody.gravityScale)); 
+        this._jumpForce = Mathf.Sqrt(jumpSpeed * -3 * (Physics2D.gravity.y * _rigidbody.gravityScale)) /2; 
     }
 
     void FixedUpdate()
     {
         transform.Translate(new Vector3(movementSpeed * Time.deltaTime, 0, 0));
     }
+    void Jump() {
+         this._rigidbody.AddForce(new Vector2(0, this._jumpForce), ForceMode2D.Impulse);
 
+    }
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) && this._onGround) {
-            this._rigidbody.AddForce(new Vector2(0, this._jumpForce), ForceMode2D.Impulse);
-        } 
+    {   
+
+        if (this._onGround && !Input.GetKeyDown(KeyCode.Space))
+        {
+            doubleJump = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (this._onGround|| doubleJump)
+            {
+                this._rigidbody.AddForce(new Vector2(0, this._jumpForce), ForceMode2D.Impulse);
+
+                doubleJump = !doubleJump;
+            }
+        }
+
+         if (Input.GetKeyDown(KeyCode.Space) )
+        {
+           this._rigidbody.AddForce(new Vector2(0, this._jumpForce), ForceMode2D.Impulse);
+        }
+
+
+        
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground") { 
             this._onGround = true;
+        } else if (other.gameObject.tag == "Obstacle") {
+            OnHitObstacle();
         }
     }
 
@@ -42,6 +71,12 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.tag == "Ground") { 
             this._onGround = false;
+        } else if (other.gameObject.tag == "Obstacle") {
+            Debug.Log("HitOUT");
+           // BackgroundScrolling.scrollSpeed = 0.1f; 
         }
     }
 }
+
+
+
