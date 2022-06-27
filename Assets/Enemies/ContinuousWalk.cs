b23c3c3360679700fcda28f12feb611ed3817e86
Wassim ben salem <bswassim@gmail.com>
2022-06-27 20:57:39 +0200

    Add background sound

--- Assets/Enemies/ContinuousWalk.cs
+++ Assets/Enemies/ContinuousWalk.cs
@@ -6,7 +6,12 @@ public class ContinuousWalk : MonoBehaviour
 {
 
     public float movementSpeed = 5;
+    void start(){
 
+    }
+    void update(){
+        
+    }
 
     // Update is called once per frame
     void FixedUpdate()
