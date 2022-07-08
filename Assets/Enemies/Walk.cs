66fcedd22257a28e65509d738c90e161f9076c63
Mario N <m.now@protonmail.com>
2022-07-08 12:52:36 +0200

    better enemy walking

---
+++ Assets/Enemies/Walk.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public bool MoveAutomatically = false;
+    protected bool moving = false;
+
+    void Start()
+    {
+        if (MoveAutomatically)
+        {
+            Move();
+        }
+    }
+
+    public void Move()
+    {
+        moving = true;
+        Debug.Log("I am now walking");
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "WarningSignSpawner")
+        {
+            Move();
+        }
+    }
+
+}
